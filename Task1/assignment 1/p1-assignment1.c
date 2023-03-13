1efa2c408fabc1b9e77dbcf62b9042118e533a45
Mahmoud <mahmoudnagdy@gmail.com>
2023-03-13 23:44:02 +0200

    Test

--- Task1/assignment 1/p1-assignment1.c
+++ Task1/assignment 1/p1-assignment1.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#define size 7
+#define size 6
 
 
 void main(){
-    int arr[] = {0, 1, 0, 2, 1, 3, 2};
-    int count = 0;
+    int arr[] = {1, 2, 3, 1, 4, 2};
+    int c = 0;
 
     for(int i = 0; i < size; i++){
         for(int j = i +  1; j < size; j++){
             if(arr[j] == arr[i]){
-                count++;
+                c++;
                 break;
             }
         }
     }
 
-    printf("%d", count);
+    printf("%d", c);
 }
