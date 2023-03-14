1f5fe32794f0cda82282d0b321fc71443ffdb599
Mahmoud Nagdy <89789838+MahmoudNagdy@users.noreply.github.com>
2023-03-14 18:40:29 +0200

    Update p.c

--- Task 3/assignment 3/p.c
+++ Task 3/assignment 3/p.c
@@ -12,5 +12,9 @@ void main(){
     
 //There Is Deference, When int in first the size was 24 and when string in first the size was 16, This is because There is padding bytes will be add to improve performance of CPU.
 //As The Size Of The Struct Changes when Sorting Of Elements Change.
+/*
+I will suppose that we have 64-bit processor, This processor does 8 byte in 1 cycle so that when int is first we will need 3 cycles and than means that we need 3 * 8 = 24 byte,
+but when string is first we will need 2 cycles and that means 2 * 8 = 16 byte.
+*/
 
 }
