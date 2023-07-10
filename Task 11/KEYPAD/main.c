365e9e5ee694862612f26d85099684505220fe06
Mahmoud <mahmoudnagdy@gmail.com>
2023-07-10 13:41:05 +0300

    Tasks

--- Task 11/KEYPAD/main.c
+++ Task 11/KEYPAD/main.c
@@ -3,132 +3,7 @@
 #include <util/delay.h>
 
 
-
-/*
-
-#define Password_Length	5
-
-
-u8 pressed[9];
-
-
-int main(void)
-{
-	u8 count;
-	u8 c = 0;
-	LCD_setPortDirection(0, 0);
-	LCD_setPortDirection(1, 0);
-	LCD_init();
-
-	// LCD_GoToXY(0, 3);
-	LCD_String_xy(0, 0, "Enter Password", &count);
-
-	while (1)
-	{
-
-
-		if (LCD_getPinValue(0, 0))
-		{
-			pressed[0] = '1';
-			LCD_GoToXY(1, 0);
-			LCD_displayCharacter('1');
-		}
-
-		else if (LCD_getPinValue(0, 5))
-		{
-			pressed[1] = '2';
-			LCD_GoToXY(1, 1);
-			LCD_displayCharacter('2');
-		}
-
-		else if (LCD_getPinValue(0, 6))
-		{
-			pressed[2] = '3';
-			LCD_GoToXY(1, 2);
-			LCD_displayCharacter('3');
-		}
-
-		else if (LCD_getPinValue(0, 7))
-		{
-			pressed[3] = '4';
-			LCD_GoToXY(1, 3);
-			LCD_displayCharacter('4');
-		}
-
-		else if (LCD_getPinValue(1, 0))
-		{
-			pressed[4] = '5';
-			LCD_GoToXY(1, 4);
-			LCD_displayCharacter('5');
-		}
-
-		else if (LCD_getPinValue(1, 2))
-		{
-			pressed[5] = '6';
-			LCD_GoToXY(1, 5);
-			LCD_displayCharacter('6');
-		}
-
-		else if (LCD_getPinValue(1, 4))
-		{
-			pressed[6] = '7';
-			LCD_GoToXY(1, 6);
-			LCD_displayCharacter('7');
-		}
-
-		else if (LCD_getPinValue(1, 5))
-		{
-			pressed[7] = '8';
-			LCD_GoToXY(1, 7);
-			LCD_displayCharacter('8');
-		}
-
-		else if (LCD_getPinValue(1, 7))
-		{
-			pressed[8] = '9';
-			LCD_GoToXY(1, 8);
-			LCD_displayCharacter('9');
-		}
-
-		else if (LCD_getPinValue(0, 2))
-				{
-					pressed[9] = '0';
-					LCD_GoToXY(1, 9);
-					LCD_displayCharacter('0');
-				}
-
-
-		if (LCD_getPinValue(1, 6))
-		{
-
-			for (int i = 0; i < Password_Length; i++)
-			{
-				for (int j = 0; j < 10; j++)
-				{
-					if (Password[i] == pressed[j])
-					{
-						c++;
-					}
-				}
-			}
-
-			if (c == Password_Length)
-			{
-				LCD_ClearScreen();
-				LCD_displayString("Correct Password");
-			}
-			else
-			{
-				LCD_ClearScreen();
-				LCD_displayString("Error");
-			}
-
-		}
-	}
-}*/
-
-
-
+#define Password_Length	6
 
 int main(void){
 	u8 count;
@@ -139,8 +14,8 @@ int main(void){
 	LCD_setPortDirection(3, 1);
 	LCD_init();
 
-	u8 pressed_Keys[5];
-	u8 password[5] = "51120";
+	u8 pressed_Keys[10];
+	u8 password[10] = "#2020#";
 
 	LCD_String_xy(0, 0, "Enter Password", &count);
 	LCD_GoToXY(1, 0);
@@ -233,7 +108,7 @@ int main(void){
 			_delay_ms(35);
 		}
 
-		if(c == 5){
+		if(c == Password_Length){
 			for(int i = 0; i < 5; i++){
 				if(pressed_Keys[i] == password[i]){
 					LCD_ClearScreen();
